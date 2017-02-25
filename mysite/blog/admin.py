07361b7caf94cf197e7e31e002e7c48a78230e72
zhuyi <670458977@qq.com>
2017-02-25 23:30:11 +0800

    生成Post模型并添加在admin后台

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
+#coding: utf-8
 from django.contrib import admin
+#导入Post表单模型
+from .models import Post
 
-# Register your models here.
+#把Post模型添加在django自带的管理页面上
+admin.site.register(Post)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,36 @@
-from __future__ import unicode_literals
+#coding:utf-8
+'''
+对象关系映射ORM
+生成Post表之后，通过QuerySet与数据库交互
+用来从数据库中增删改查，排序过滤等操作
+最后我们的Post模型还需通过admin在django自带的web管理界面上生成
 
+'''
 from django.db import models
+from django.utils import timezone # 获取系统时间
+from django.contrib.auth.models import User #创建用户
 
-# Create your models here.
+#models模块内的每一个class对应数据库内的一张表格
+class Post(models.Model):
+	#作者为主键，和用户绑定
+	author = models.ForeignKey(User)
+	#标题为普通文本字段，长度限制最大100
+	title = models.CharField(max_length= 100)
+	#内容text字段，长文本
+	text = models.TextField()
+	#创建日期
+	created_date = models.DateTimeField(default= timezone.now)
+	#发布日期
+	published_date = models.DateTimeField(blank= True, null=True)
+
+	#保存时间为发布日期
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
+
+
+
